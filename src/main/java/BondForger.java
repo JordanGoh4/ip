64fd39eec39f7df9ecacbcc7a5f3d6e5414b31ef
JordanGoh4 <lowonmath@gmail.com>
2026-01-22 23:30:25 +0800

    Rename main class

---
+++ src/main/java/BondForger.java
@@ -0,0 +1,12 @@
+public class BondForger {
+    public static void main(String[] args) {
+        String name = "Bond Forger";
+        String logo = " ___________________________\n"
+                + "Hello! I'm " +  name + "\n"
+                + "What can I do for you?\n"
+                + " ___________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + " ___________________________\n";
+        System.out.println(logo);
+    }
+}
